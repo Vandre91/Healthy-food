38260294f7b73dad0d6a52aaa3b6ba08d6ec5ace
Victor <victor.andre900@gmail.com>
2016-04-12 11:39:49 +0200

    designe recipes

--- Healthyfood/Healthyfood/UserControl_Recipe.Designer.cs
+++ Healthyfood/Healthyfood/UserControl_Recipe.Designer.cs
@@ -37,10 +37,9 @@
             // 
             // button_Back
             // 
-            this.button_Back.Location = new System.Drawing.Point(653, 471);
-            this.button_Back.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Back.Location = new System.Drawing.Point(490, 383);
             this.button_Back.Name = "button_Back";
-            this.button_Back.Size = new System.Drawing.Size(100, 28);
+            this.button_Back.Size = new System.Drawing.Size(75, 23);
             this.button_Back.TabIndex = 0;
             this.button_Back.Text = "Retour";
             this.button_Back.UseVisualStyleBackColor = true;
@@ -49,10 +48,12 @@
             // button1_recipe_health
             // 
             this.button1_recipe_health.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1_recipe_health.BackgroundImage")));
+            this.button1_recipe_health.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button1_recipe_health.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1_recipe_health.Location = new System.Drawing.Point(243, 86);
+            this.button1_recipe_health.Location = new System.Drawing.Point(182, 70);
+            this.button1_recipe_health.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1_recipe_health.Name = "button1_recipe_health";
-            this.button1_recipe_health.Size = new System.Drawing.Size(300, 92);
+            this.button1_recipe_health.Size = new System.Drawing.Size(225, 75);
             this.button1_recipe_health.TabIndex = 1;
             this.button1_recipe_health.Text = "Recettes santé";
             this.button1_recipe_health.UseVisualStyleBackColor = true;
@@ -61,10 +62,12 @@
             // button2_yours_recipes
             // 
             this.button2_yours_recipes.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2_yours_recipes.BackgroundImage")));
+            this.button2_yours_recipes.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button2_yours_recipes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2_yours_recipes.Location = new System.Drawing.Point(243, 221);
+            this.button2_yours_recipes.Location = new System.Drawing.Point(182, 180);
+            this.button2_yours_recipes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2_yours_recipes.Name = "button2_yours_recipes";
-            this.button2_yours_recipes.Size = new System.Drawing.Size(300, 92);
+            this.button2_yours_recipes.Size = new System.Drawing.Size(225, 75);
             this.button2_yours_recipes.TabIndex = 2;
             this.button2_yours_recipes.Text = "Vos recettes";
             this.button2_yours_recipes.UseVisualStyleBackColor = true;
@@ -72,10 +75,12 @@
             // button3_world_recipes
             // 
             this.button3_world_recipes.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3_world_recipes.BackgroundImage")));
-            this.button3_world_recipes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3_world_recipes.Location = new System.Drawing.Point(243, 368);
+            this.button3_world_recipes.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3_world_recipes.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3_world_recipes.Location = new System.Drawing.Point(182, 299);
+            this.button3_world_recipes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3_world_recipes.Name = "button3_world_recipes";
-            this.button3_world_recipes.Size = new System.Drawing.Size(300, 92);
+            this.button3_world_recipes.Size = new System.Drawing.Size(225, 75);
             this.button3_world_recipes.TabIndex = 3;
             this.button3_world_recipes.Text = "         Recettes du monde";
             this.button3_world_recipes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -83,7 +88,7 @@
             // 
             // UserControl_Recipe
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
@@ -91,9 +96,8 @@
             this.Controls.Add(this.button2_yours_recipes);
             this.Controls.Add(this.button1_recipe_health);
             this.Controls.Add(this.button_Back);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "UserControl_Recipe";
-            this.Size = new System.Drawing.Size(831, 550);
+            this.Size = new System.Drawing.Size(623, 447);
             this.ResumeLayout(false);
 
         }
