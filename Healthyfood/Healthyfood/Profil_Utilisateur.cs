0c583186948156bc29e91464d17d70d3f86e0a5a
abcisse <abcisse@intechinfo.fr>
2016-04-27 10:39:05 +0200

    Profil_Utilisateur

--- Healthyfood/Healthyfood/Profil_Utilisateur.cs
+++ Healthyfood/Healthyfood/Profil_Utilisateur.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace Healthyfood
 {
-    class Profil_Utilisateur
+    public class Profil_Utilisateur
     {
+        string _firstName;
+        string _lastName;
+        int _age;
+        int _weight; 
+        double _height; // un réel de tel sorte que 1.8 correspond à 180 cm
+        bool _isFemale;
+        int _imc;
+
+        Profil_Utilisateur (string firstName, string lastName, int age , int weight, double height , bool isFemale)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _age = age;
+            _weight = weight;
+            _height = height;
+            _isFemale = isFemale;
+        }
+
+        string FirstName
+        { get { return _firstName; } }
+
+        string LastName
+        { get { return _lastName; } }
+
+        int Age
+        { get { return _age; } }
+
+        int Weight
+        { get { return _weight; } }
+
+        double Height
+        { get { return _height; } }
+
+        double Imc
+        {
+            get { return (_weight / (_height * _height)) ; }
+        }
     }
 }
