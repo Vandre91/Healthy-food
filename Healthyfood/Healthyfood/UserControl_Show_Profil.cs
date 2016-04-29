3573f2eb26c9c02ec93d09d4b5b6b54b4c754030
Victor <victor.andre900@gmail.com>
2016-04-29 10:18:24 +0200

    modify profil

---
+++ Healthyfood/Healthyfood/UserControl_Show_Profil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Healthyfood
+{
+    public partial class UserControl_Show_Profil : UserControl
+    {
+        public UserControl_Show_Profil()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Back_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+        }
+
+        private void button_Save_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
