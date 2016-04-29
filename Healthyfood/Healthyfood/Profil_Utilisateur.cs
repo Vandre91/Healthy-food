42b296993a3d95030f5dc3b9023395fd6497ea35
abcisse <abcisse@intechinfo.fr>
2016-04-29 11:20:15 +0200

    Healthyfood Tests / public utilisateurs/ exceptions firstname and name / exceptions Profil_Utilisateur

--- Healthyfood/Healthyfood/Profil_Utilisateur.cs
+++ Healthyfood/Healthyfood/Profil_Utilisateur.cs
@@ -15,16 +15,14 @@ namespace Healthyfood
         string _firstName;
         string _lastName;
         int _age;
-        int _weight; 
+        int _weight;
         double _height; // un réel de tel sorte que 1.8 correspond à 180 cm
         bool _isFemale;
         double _imc;
 
-        internal Profil_Utilisateur (string firstName, string lastName, int age , int weight, double height , bool isFemale)
+        internal Profil_Utilisateur(string firstName, string lastName, int age, int weight, double height, bool isFemale)
         {
-            if (firstName == null || firstName == string.Empty || string.IsNullOrWhiteSpace(firstName)) throw new ArgumentException("The name must not be empty", nameof(firstName));
-            if (lastName == null || lastName == string.Empty || string.IsNullOrWhiteSpace(lastName)) throw new ArgumentException("The name must not be empty", nameof(lastName));
-            if (height < 1.0 || height > 2.3) throw new ArgumentException("The height does not match", nameof(height));
+
             _firstName = firstName;
             _lastName = lastName;
             _age = age;
@@ -50,49 +48,58 @@ namespace Healthyfood
         }
 
         string FirstName
-        { get { return _firstName; }
-            set {
+        {
+            get { return _firstName; }
+            set
+            {
                 if (value == null || value == string.Empty || string.IsNullOrWhiteSpace(value)) throw new ArgumentException("The name must not be empty", nameof(value));
                 _firstName = value;
-                }
+            }
         }
 
-        string LastName
+        internal string LastName
         {
             get { return _lastName; }
-            set {
+            set
+            {
                 if (value == null || value == string.Empty || string.IsNullOrWhiteSpace(value)) throw new ArgumentException("The name must not be empty", nameof(value));
                 _lastName = value;
-                }
+            }
         }
 
         int Age
-        { get { return _age; }
-          set {
+        {
+            get { return _age; }
+            set
+            {
                 if (value < 0 || value > 130) throw new ArgumentException("The age must be more than 0 and less than 130", nameof(value));
                 _age = value;
-              }
+            }
         }
 
         int Weight
-        { get { return _weight; }
-            set {
+        {
+            get { return _weight; }
+            set
+            {
                 if (value < 2 || value > 230) throw new ArgumentException("The weight must be more than 2 and less than 230", nameof(value));
                 _weight = value;
-                }
+            }
         }
 
         double Height
-        { get { return _height; }
-            set {
+        {
+            get { return _height; }
+            set
+            {
                 if (value < 30 || value > 250) throw new ArgumentException("The weight must be more than 30 and less than 250", nameof(value));
                 _height = value;
-                }
+            }
         }
 
         double Imc
         {
-            get { return (_weight / (_height * _height)/100) ; }
+            get { return (_weight / (_height * _height) / 100); }
         }
     }
 }
--- Healthyfood/Healthyfood/Utilisateurs.cs
+++ Healthyfood/Healthyfood/Utilisateurs.cs
@@ -14,9 +14,9 @@ namespace Healthyfood
         Dictionary<string, Profil_Utilisateur> Famille;
         int _count; // cinq utilisateurs au maximum
 
-        internal Utilisateurs ()
+        public Utilisateurs()
         {
-            Famille = new Dictionary<string, Profil_Utilisateur>() ;
+            Famille = new Dictionary<string, Profil_Utilisateur>();
         }
         public void save(string path)
         {
@@ -27,31 +27,37 @@ namespace Healthyfood
             }
         }
 
-        public Profil_Utilisateur CreateUser (string firstName, string lastName, int age, int weight, double height, bool isFemale)
+        public Profil_Utilisateur CreateUser(string firstName, string lastName, int age, int weigth, double heigth, bool isFemale)
         {
-            if (Famille.ContainsKey(firstName)) throw new ArgumentException("this name must be different because it already exists", nameof(firstName));
-            if (_count > 5) throw new ArgumentException("users must be less than five", nameof(_count));
+
+            if (_count == 5) throw new ArgumentException("users must be less than five", nameof(_count));
             if (firstName == null || firstName == string.Empty || string.IsNullOrWhiteSpace(firstName)) throw new ArgumentException("The name must not be empty", nameof(firstName));
             if (lastName == null || lastName == string.Empty || string.IsNullOrWhiteSpace(lastName)) throw new ArgumentException("The name must not be empty", nameof(lastName));
-            if (height < 1.0 || height > 2.3) throw new ArgumentException("The height does not match", nameof(height));
-            Profil_Utilisateur u = new Profil_Utilisateur(firstName, lastName, age, weight, height, isFemale);
+            if (Famille.ContainsKey(firstName))
+            {
+                Profil_Utilisateur verification = Famille[firstName];
+                if (verification.LastName == lastName) throw new ArgumentException("this name must be different because it already exists", nameof(lastName));
+            }
+            if (heigth < 30 || heigth > 230) throw new ArgumentException("The height does not match", nameof(heigth));
+            if (weigth < 0 || weigth > 250) throw new ArgumentException("The height does not match", nameof(weigth));
+            Profil_Utilisateur u = new Profil_Utilisateur(firstName, lastName, age, weigth, heigth, isFemale);
             Famille[firstName] = u;
             _count++;
             return u;
         }
 
-        public Profil_Utilisateur FindUser (string firstName)
+        public Profil_Utilisateur FindUser(string firstName)
         {
             return Famille.ContainsKey(firstName) ? Famille[firstName] : null;
         }
 
-        public void RemoveUser (string firstName)
+        public void RemoveUser(string firstName)
         {
             if (Famille.ContainsKey(firstName))
-                {
+            {
                 Famille.Remove(firstName);
                 _count--;
-                }
+            }
         }
     }
 }
