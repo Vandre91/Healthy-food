c21a4fd2f7eb362b107a2bec5dbbbca60fe0f744
KABOBO <kaboboandrian@yahoo.fr>
2016-04-29 11:04:01 +0200

    Add class Drink

---
+++ Healthyfood/StockFood/Drink.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockFood
+{
+   public  class Drink
+    {
+        string _name;
+        double _amount;
+        double _balance;
+        DateTime _dayOfbuy;
+        DateTime _expirationday;
+    }
+}
