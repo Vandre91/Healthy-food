463cb29b3f52fdf4dba5ffc28a3115efd08c1721
abcisse <abcisse@intechinfo.fr>
2016-05-11 11:14:12 +0200

    T2Stocks tests suite et fin/ Ingredients reduce/stocks functions

--- Healthyfood/HealthyFood.Tests/T2Stocks.cs
+++ Healthyfood/HealthyFood.Tests/T2Stocks.cs
@@ -62,6 +62,56 @@ namespace HealthyFoodTests
 
         }
 
-    }
+        [Test]
+        public void t04_remove_ingredient_correctly()
+        {
+            Stocks stocks = new Stocks();
+            DateTime date = new DateTime(2017, 10, 10);
+            DateTime date1 = new DateTime(2018, 10, 10);
+            Ingredients i = stocks.AddIngredient("viande", "Steak de boeuf", 100, date);
+            Ingredients u = new Ingredients("viande", "cote de boeuf", 100, date);
+            Assert.Throws<ArgumentException>(() => stocks.RemoveIngredient("cote de boeuf", date));
+            Assert.Throws<ArgumentException>(() => stocks.RemoveIngredient("Steak de boeuf", date1));
+            stocks.RemoveIngredient("Steak de boeuf", date);
+            Assert.Throws<ArgumentException>(() => stocks.RemoveIngredient("Steak de boeuf", date));
+            Assert.Throws<ArgumentException>(() => stocks.RemoveIngredient("Steak de chien", date));
 
+        }
+
+        [Test]
+        public void t05_add_ingredient_which_already_exist ()
+        {
+            Stocks stocks = new Stocks();
+            DateTime date = new DateTime(2017, 10, 10);
+            Ingredients i = stocks.AddIngredient("viande", "Steak de boeuf", 100, date);
+            stocks.AddIngredient("viande", "Steak de boeuf", 150, date);
+            Assert.That(Stocks._stock[Stocks.naming(i)].Balance, Is.EqualTo(250));
+        }
+
+        [Test]
+        public void t06_reduce_ingredient_correctly()
+        {
+            Stocks stocks = new Stocks();
+            DateTime date = new DateTime(2017, 10, 10);
+            Ingredients i = stocks.AddIngredient("viande", "Steak de boeuf", 100, date);
+            Ingredients u = stocks.AddIngredient("viande", "cote de boeuf", 100, date);
+            Assert.Throws<ArgumentException>(() => stocks.ReduceIngredient("Steak de boeuf",200, date));
+            Assert.Throws<ArgumentException>(() => stocks.ReduceIngredient("Steak de chat", 200, date));
+            Assert.Throws<ArgumentException>(() => stocks.ReduceIngredient("Steak de boeuf", -1, date));
+            Assert.That(Stocks._stock[Stocks.naming(i)].Balance, Is.EqualTo(100));
+            stocks.ReduceIngredient("Steak de boeuf", 50, date);
+            Assert.That(Stocks._stock[Stocks.naming(i)].Balance, Is.EqualTo(50));
+        }
+
+        [Test]
+        public void t07_create_new_ingredient ()
+        {
+            Stocks stocks = new Stocks();
+            DateTime date = new DateTime(2017, 10, 10);
+            Assert.Throws<ArgumentException>(() => stocks.CreateIngredient("viande", "lait"));
+            stocks.CreateIngredient("viande", "steak de chien");
+            Assert.That(Ingredients.viande.Contains("steak de chien"), Is.EqualTo(true));
+        }
+    }
 }
+ 
