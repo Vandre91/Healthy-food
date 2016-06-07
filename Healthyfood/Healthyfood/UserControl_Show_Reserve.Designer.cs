0c483f146dc7f39e6bdf02bacfbe4e717cbb0f91
abcisse <abcisse@intechinfo.fr>
2016-06-07 11:07:00 +0200

    Reserve

--- Healthyfood/Healthyfood/UserControl_Show_Reserve.Designer.cs
+++ Healthyfood/Healthyfood/UserControl_Show_Reserve.Designer.cs
@@ -33,14 +33,17 @@
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader_Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader_Quantity = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader_Date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button_Remove
             // 
             this.button_Remove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button_Remove.Location = new System.Drawing.Point(304, 321);
+            this.button_Remove.Location = new System.Drawing.Point(405, 395);
+            this.button_Remove.Margin = new System.Windows.Forms.Padding(4);
             this.button_Remove.Name = "button_Remove";
-            this.button_Remove.Size = new System.Drawing.Size(105, 59);
+            this.button_Remove.Size = new System.Drawing.Size(140, 73);
             this.button_Remove.TabIndex = 1;
             this.button_Remove.Text = "Suprimer";
             this.button_Remove.UseVisualStyleBackColor = true;
@@ -49,9 +52,10 @@
             // button_Back
             // 
             this.button_Back.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button_Back.Location = new System.Drawing.Point(415, 321);
+            this.button_Back.Location = new System.Drawing.Point(553, 395);
+            this.button_Back.Margin = new System.Windows.Forms.Padding(4);
             this.button_Back.Name = "button_Back";
-            this.button_Back.Size = new System.Drawing.Size(105, 59);
+            this.button_Back.Size = new System.Drawing.Size(140, 73);
             this.button_Back.TabIndex = 2;
             this.button_Back.Text = "Retour";
             this.button_Back.UseVisualStyleBackColor = true;
@@ -64,10 +68,12 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader_Name,
-            this.columnHeader_Quantity});
-            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.columnHeader_Quantity,
+            this.columnHeader_Date});
+            this.listView1.Location = new System.Drawing.Point(4, 4);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(295, 377);
+            this.listView1.Size = new System.Drawing.Size(392, 463);
             this.listView1.TabIndex = 3;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -82,17 +88,35 @@
             this.columnHeader_Quantity.Text = "Quantité";
             this.columnHeader_Quantity.Width = 72;
             // 
+            // columnHeader_Date
+            // 
+            this.columnHeader_Date.Text = "Date";
+            this.columnHeader_Date.Width = 100;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(666, 357);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 4;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
             // UserControl_Show_Reserve
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button_Back);
             this.Controls.Add(this.button_Remove);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "UserControl_Show_Reserve";
-            this.Size = new System.Drawing.Size(526, 383);
+            this.Size = new System.Drawing.Size(701, 471);
             this.Enter += new System.EventHandler(this.UserControl_Show_Reserve_Enter);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -103,5 +127,7 @@
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader columnHeader_Name;
         private System.Windows.Forms.ColumnHeader columnHeader_Quantity;
+        private System.Windows.Forms.ColumnHeader columnHeader_Date;
+        private System.Windows.Forms.Label label1;
     }
 }
